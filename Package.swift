fce19e32d815d011745c7854365b65ed27fe9a1a
David Lundgren <dealundgren@gmail.com>
2023-05-05 13:24:36 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,11 +10,6 @@ let package = Package(
             targets: ["SPMPOCFramework"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "SPMPOCFramework"
-        ),
         .binaryTarget(
             name: "SPMPOCFramework",
             path: "/SPMPOCFramework.xcframework"
