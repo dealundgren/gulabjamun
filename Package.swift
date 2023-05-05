279bc04189b8c52096bce0c6c9915a8121a75eb4
David Lundgren <dealundgren@gmail.com>
2023-05-05 13:17:55 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
+    name: "SPMPOCFramework",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
